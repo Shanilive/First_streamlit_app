2ee54d95778623ba082c4fcd23d82b575da87105
Shani Hettiarachchi <102456983+Shanilive@users.noreply.github.com>
2022-07-21 12:32:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 
-streamlit.header('Main Breakfast Menu')
+streamlit.header('🥣 Main Breakfast Menu')
 
-streamlit.text('Freanch Toast')
-streamlit.text('Blue berry smoothy')
-streamlit.text('Fried Egg')
-streamlit.text('Bacon')
+streamlit.text('🥗 Freanch Toast')
+streamlit.text('🐔 Blue berry smoothy')
+streamlit.text('🥑 Fried Egg')
+streamlit.text('🍞 Bacon')
