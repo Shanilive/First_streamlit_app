70109e587f8d5b3acb9b13b9771c1a29ac5ee807
Shani Hettiarachchi <102456983+Shanilive@users.noreply.github.com>
2022-07-21 11:50:37 +0530

    Update streamlit_app.py
    added title with streamlit.title   and added text with streamlit.text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 
-streamlit.title('Main Menu')
+streamlit.title('Main Breakfast Menu')
 
 streamlit.text('Freanch Toast')
+streamlit.text('Blue berry smoothy')
+streamlit.text('Fried Egg')
+streamlit.text('Bacon')
