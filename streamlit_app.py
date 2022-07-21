f5bc5b3c3ac83a6ad46bbfceb8d357a6c0ccf73e
Shani Hettiarachchi <102456983+Shanilive@users.noreply.github.com>
2022-07-21 12:53:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
 
-streamlit.header('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.header('🥣 Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard Boilled Free ranged Edd')
 streamlit.text('🥑 Avocado Toast')
